f9eb33cbd725dfda71441a514c36698badafd640
Josh-Westbrook <116318801+Josh-Westbrook@users.noreply.github.com>
2026-01-17 15:57:45 -0600

    Anshu code through Josh's computer

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuild_2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 2;
-    public static final String GIT_SHA = "3da1e7bd76bd8ceac17a2f57f143ddfee77c7ebf";
-    public static final String GIT_DATE = "2026-01-16 14:53:48 EST";
+    public static final int GIT_REVISION = 3;
+    public static final String GIT_SHA = "149d2cbc77749d28c3648879e5e27c3b961861ef";
+    public static final String GIT_DATE = "2026-01-17 15:43:27 EST";
     public static final String GIT_BRANCH = "QuestNav2026";
-    public static final String BUILD_DATE = "2026-01-17 15:42:58 EST";
-    public static final long BUILD_UNIX_TIME = 1768682578631L;
+    public static final String BUILD_DATE = "2026-01-17 16:09:56 EST";
+    public static final long BUILD_UNIX_TIME = 1768684196088L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
