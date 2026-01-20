d53785a035c2660e6693449d07f90c2474038aad
ModBoyex <79735432+ModBoyex@users.noreply.github.com>
2026-01-20 15:09:43 -0600

    Add last year robot model + this year place holder

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuild_2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 2;
-    public static final String GIT_SHA = "3da1e7bd76bd8ceac17a2f57f143ddfee77c7ebf";
-    public static final String GIT_DATE = "2026-01-16 14:53:48 EST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-01-17 13:05:48 EST";
-    public static final long BUILD_UNIX_TIME = 1768673148224L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 3;
+    public static final String GIT_SHA = "ba10bc0450b56073d0241616bb8df4182b05b248";
+    public static final String GIT_DATE = "2026-01-17 13:47:11 EST";
+    public static final String GIT_BRANCH = "AdvantageScope-Models";
+    public static final String BUILD_DATE = "2026-01-20 16:07:45 EST";
+    public static final long BUILD_UNIX_TIME = 1768943265593L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
