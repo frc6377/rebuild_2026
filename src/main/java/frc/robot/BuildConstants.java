e09731ff2d63a6e0c53d0a15fece896b65fd83d5
redjumper2 <arjun.mijar@gmail.com>
2026-01-24 16:40:51 -0600

    Update BuildConstants for versioning and build metadata

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuilt_2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 7;
-    public static final String GIT_SHA = "64e6041625b9a451f8bc1b9f38f9d5ad00022288";
-    public static final String GIT_DATE = "2026-01-24 16:45:10 EST";
+    public static final int GIT_REVISION = 8;
+    public static final String GIT_SHA = "e16211ee2204f7adfd44aacef9ff3be12d40cf5e";
+    public static final String GIT_DATE = "2026-01-24 16:48:27 EST";
     public static final String GIT_BRANCH = "ClimbSubsystem";
-    public static final String BUILD_DATE = "2026-01-24 16:47:35 EST";
-    public static final long BUILD_UNIX_TIME = 1769291255088L;
+    public static final String BUILD_DATE = "2026-01-24 16:50:26 EST";
+    public static final long BUILD_UNIX_TIME = 1769291426530L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
