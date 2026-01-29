14e96acabfc971ad6029b28ed4f1717e4ca05793
ModBoyex <79735432+ModBoyex@users.noreply.github.com>
2026-01-28 20:34:54 -0600

    Add Last year's gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuild_2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 2;
-    public static final String GIT_SHA = "3da1e7bd76bd8ceac17a2f57f143ddfee77c7ebf";
-    public static final String GIT_DATE = "2026-01-16 14:53:48 EST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-01-17 13:05:48 EST";
-    public static final long BUILD_UNIX_TIME = 1768673148224L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 6;
+    public static final String GIT_SHA = "faeddb2d5124c2ba21cab198fbfa336ce918130b";
+    public static final String GIT_DATE = "2026-01-28 21:33:22 EST";
+    public static final String GIT_BRANCH = "Advantage-Scope-Models";
+    public static final String BUILD_DATE = "2026-01-28 21:33:31 EST";
+    public static final long BUILD_UNIX_TIME = 1769654011705L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
